e5f7baee939627c96b241c726b5503ca4cb178e4
babarali7 <babar_ali009@hotmail.com>
2025-02-13 16:08:27 +0500

    upgrade to laravel 10

---
+++ database/migrations/2025_02_10_101744_create_lab_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lab', function (Blueprint $table) {
+            $table->increments('lab_id');
+            $table->string('lab_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lab');
+    }
+}
